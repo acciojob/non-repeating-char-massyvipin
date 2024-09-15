6a7ff28949b7da70a5de82b32c9c71b39a1407be
massyvipin <166917148+massyvipin@users.noreply.github.com>
2024-09-15 20:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	const charFeqCount = {};
+ for (let char of str) {
+ 	if (charFeqCount[char]) {
+ 		charFeqCount[char]+=1;
+ 	}
+	 else{
+		 charFeqCount[char] = 1;
+	 }
+ }
+	for(let char of str){
+		if(charFeqCount[char]===1){
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
